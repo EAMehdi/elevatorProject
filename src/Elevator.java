793f2c749addee46527e7781a05198d444215e23
zakarya615 <58936648+zakarya615@users.noreply.github.com>
2019-12-18 15:02:38 +0100

    Création classe Elevator
    Controleur/Get/Set

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,28 +1,65 @@
-public class Elevator{
+public class Elevator
+{
   private static int count = 0;
-  private int idElevator, currentStep, minStep, maxStep, nbPassagers;
+  private int idElevator, currentFloor, minFloor, maxFloor, nbPassagers, maxPassagers, velocity;
   private boolean working;
   private enum state{wait,goUp,goDown};
 
-  public Elevator(){
-    setIdElevator(count++);
-    working = true;
-  }
-  public Elevator(int min, int max){
-    this();
-    this.minStep=min;
-    this.minStep=max;
+  public Elevator(int currentF, int maxF, int nbrPa, int maxP, int vly)
+  {
+    this.idElevator= this.count++;
+    this.currentFloor=currentF;
+    this.minFloor= 0; // les étages sont numérotés de 0 à n
+    this.maxFloor=maxF;
+    this.nbPassagers=nbrPa;
+    this.maxPassagers=maxP;
+    this.velocity=vly;
   }
 
-  public int getIdElevator(){
+  public int getIdElevator() // Retourne l'id de l'ascenseur (int)
+  {
     return this.idElevator;
   }
 
-  public void setIdElevator(int id){
-    this.idElevator = id;
+  public int getCurrentFloor() // Retourne l'étage où se situe l'ascenseur (int)
+  {
+    return this.currentFloor;
+  }
+  public void setCurrentFloor(int newCurrentFloor)
+  {
+    this.currentFloor=newCurrentFloor;
   }
 
-  public int getCurrentStep(){
-    return currentStep;
+  public int getMinFloor() //Retourne l'étage le plus bas (int)
+  {
+    return this.minFloor;
+  }
+
+  public int getMaxFloor() //Retourne l'étage le plus haut (int)
+  {
+    return this.maxFloor;
+  }
+  public void setMaxFloor(int newMaxFloor)
+  {
+    this.maxFloor=newMaxFloor;
+  }
+
+  public int getNbPassagers() //Retourne le nombre de passagers dans cet ascenseur (int)
+  {
+    return this.nbPassagers;
+  }
+  public void setNbPassagers(int newNbPassagers)
+  {
+    this.nbPassagers=newNbPassagers;
+  }
+
+  public int getMaxPassagers() //Retourne le nombre de passagers maximum par ascenseur (int)
+  {
+    return this.maxPassagers  ;
+  }
+
+  public int getVelocity() //Retourne le nombre de pas de temps nécessaire passer à l'étage suivant ou précédant.
+  {
+    return this.velocity;
   }
 }
--- src/Personne.java
+++ src/Personne.java
@@ -1,4 +1,5 @@
-public class Personne{
+public class Personne
+{
   private static int count = 0;
   private int idPersonne, step, startFloor, endFloor;
 
@@ -9,12 +10,9 @@ public class Personne{
     this.endFloor=theEndFloor;
   }
 
-  public int getIdPersonne(){
+  public int getIdPersonne()
+  {
     return this.idPersonne;
-  }
-
-  private void setIdPersonne(int id){
-    this.idPersonne= id;
   }
 
   public int getStep()
